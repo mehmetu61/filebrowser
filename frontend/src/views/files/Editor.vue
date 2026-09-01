d9f35b48d6eaa5516155506e03bae9703842bb48
mehmetu61 <mehmetu61@gmx.de>
2026-09-01 14:42:30 +0300

    refactor: import watch from vue in Editor component

--- frontend/src/views/files/Editor.vue
+++ frontend/src/views/files/Editor.vue
@@ -109,7 +109,7 @@ import { useLayoutStore } from "@/stores/layout";
 import { getEditorTheme } from "@/utils/theme";
 import { marked } from "marked";
 import markedKatex from "marked-katex-extension";
-import { computed, inject, onBeforeUnmount, onMounted, ref, watchEffect } from "vue";
+import { computed, inject, onBeforeUnmount, onMounted, ref, watch, watchEffect } from "vue";
 import { useI18n } from "vue-i18n";
 import { onBeforeRouteUpdate, useRoute, useRouter } from "vue-router";
 import { read, copy } from "@/utils/clipboard";
